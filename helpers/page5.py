6617598eff7a3c2d5425535ad976af2c78387efd
aldemir.neto <humbertoaldemir@gmail.com>
2024-06-08 14:09:47 -0300

    tirando lixo de codigo

--- helpers/page5.py
+++ helpers/page5.py
@@ -10,28 +10,26 @@ def insert_interaction(interaction_id, score):
         # Connect to the database
         conn = duckdb.connect()
 
-        st.write('instalando postgres...')
+
         conn.execute('INSTALL postgres;')
         # Assuming the PostgreSQL extension is pre-installed and just needs to be loaded
 
         conn.execute('LOAD postgres;')
-        st.write('carregou postgres')
+
         # Securely format the connection string
         db_connection_string = f"postgresql://postgres.{os.environ['DB_ID']}:{os.environ['DB_PASSWORD']}@aws-0-{os.environ['DB_REGION']}.pooler.supabase.com:{os.environ['DB_PORT']}/postgres"
 
-        st.write('linkando...')
+
         # Attach the connection
         conn.execute(f"""
         ATTACH '{db_connection_string}' AS db (TYPE postgres);
         """)
-        st.write('nao deu certo pra linkar')
 
-        st.write('inserindo...')
         conn.execute(f"""
         INSERT INTO db.user_interaction (interaction_id, score)
         VALUES ('{interaction_id}', '{score}');
         """)
-        st.write('inseriu')
+
         conn.close()
         return 'Email inserido com sucesso'
 
