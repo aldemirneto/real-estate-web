ff1ac980efdeaa14c3a5bd66736ee0da90d951f6
aldemir.neto <humbertoaldemir@gmail.com>
2024-06-08 11:41:20 -0300

    alterando metodo de conexao db

--- database/db.py
+++ database/db.py
@@ -3,9 +3,16 @@ import os
 from langchain_community.utilities import SQLDatabase
 from sqlalchemy import create_engine
 
-engine = create_engine(f'postgresql://postgres.{os.environ["DB_ID"]}:{os.environ["DB_PASSWORD"]}@aws-0-{os.environ["DB_REGION"]}.pooler.supabase.com:{os.environ["DB_PORT"]}/postgres')
 
-db = SQLDatabase(engine=engine, view_support=True)
+url = f'postgresql://postgres.{os.environ["DB_ID"]}:{os.environ["DB_PASSWORD"]}@aws-0-{os.environ["DB_REGION"]}.pooler.supabase.com:{os.environ["DB_PORT"]}/postgres'
+
+db = SQLDatabase.from_uri(
+    url,
+    schema="public",
+    include_tables=['real_estate_data'],
+    sample_rows_in_table_info=1,
+    view_support=True
+)
 
 
 def get_schema(_):
